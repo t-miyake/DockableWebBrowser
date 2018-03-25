f1a9987e5e259568c76852b680a040298a5f1382
T.Miyake <info@miyake.me>
2018-03-25 17:35:14 +0900

    Initial commit.

---
+++ DockableWebBrowser/Dockable_Web_Browser.cs
@@ -0,0 +1,34 @@
+﻿namespace DockableWebBrowser
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("f9c297fb-d3e5-4d05-b218-90e055d7cf6c")]
+    public class Dockable_Web_Browser : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dockable_Web_Browser"/> class.
+        /// </summary>
+        public Dockable_Web_Browser() : base(null)
+        {
+            this.Caption = "Dockable Web Browser";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new Dockable_Web_BrowserControl();
+        }
+    }
+}
---
+++ DockableWebBrowser/Dockable_Web_BrowserControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace DockableWebBrowser
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for Dockable_Web_BrowserControl.
+    /// </summary>
+    public partial class Dockable_Web_BrowserControl : UserControl
+    {
+        public const string FeatureBrowserEmulation = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+        private readonly Microsoft.Win32.RegistryKey _regkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(FeatureBrowserEmulation);
+        readonly string _myName = System.IO.Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dockable_Web_BrowserControl"/> class.
+        /// </summary>
+        public Dockable_Web_BrowserControl()
+        {
+            this.InitializeComponent();
+            _regkey.SetValue(_myName, 11001, Microsoft.Win32.RegistryValueKind.DWord);
+            _regkey.Close();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "Dockable Web Browser");
+        }
+    }
+}
